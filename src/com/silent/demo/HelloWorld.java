dd2c5aa77dd83f593d8f836328ed70152f025e52
王康瑞 <wangkangrui@sinosoft.com.cn>
2017-08-29 21:10:12 +0800

    hoho

---
+++ src/com/silent/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.silent.demo;
+
+/**
+ * Created by wkr on 2017/8/28.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("HelloWorld");
+    }
+}
